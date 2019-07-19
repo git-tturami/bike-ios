774d32c93674ded2fb1e6ce243219d4544e6dbef
hihiboss <junhui820@gmail.com>
2019-07-19 21:55:49 +0900

    #0 [Project] Set iOS project and apply SwiftLint

---
+++ bike-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bike-ios
+//
+//  Created by JunHui Kim on 19/07/2019.
+//  Copyright © 2019 gitturami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
