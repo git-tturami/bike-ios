240d8855b45a13719c63658859c5bdd4841388df
hihiboss <junhui820@gmail.com>
2019-09-08 17:47:18 +0900

    ios#4 [END] Frame end screen structure

---
+++ bike-ios/End/Model/Result.swift
@@ -0,0 +1,13 @@
+//
+//  Result.swift
+//  bike-ios
+//
+//  Created by JunHui Kim on 08/09/2019.
+//  Copyright © 2019 gitturami. All rights reserved.
+//
+
+import Foundation
+
+struct Result {
+    
+}
